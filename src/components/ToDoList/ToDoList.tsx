e41e85281a3c9c6a3c00129a19d7285dea7735e3
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2024-04-25 12:26:36 +0300

    fix

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { selectTodoList } from '../../redux/todos/selectors';
 
 import Card from '../Card/Card';
